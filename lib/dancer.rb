e0af01e883a5c7b8ed2841f5f336b4117d89c9e3
senadakadric <skadric@fordham.edu>
2020-05-17 12:25:47 -0400

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,20 @@
-require_relative './class_methods_module.rb'
-require_relative './dance_module.rb'
+# require_relative './class_methods_module.rb'
+# require_relative './dance_module.rb'
+require_relative './fancy_dance.rb'
+
+class Dancer
+    # initial way of including Dance instance methods and MetaDancing class methods:
+    # include Dance
+    # extend MetaDancing
+
+    #refactored way:
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+end
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,23 @@
-require_relative './dance_module.rb'
-require_relative './class_methods_module.rb'
+# require_relative './dance_module.rb'
+# require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid
+    #initial way
+    #if we use the include keyword, we allow our class to use all of the methods of the included module (in this case Dance, found in the dance_module.rb file as instance methods
+    # include Dance
+
+    # #in order to lend a module's methods to a class as class methods, we use the extend keyword
+    # extend MetaDancing
+
+    #refactored way
+    extend FancyDance::ClassMethods
+    include FancyDance::InstanceMethods
+
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+    end
+
+end
